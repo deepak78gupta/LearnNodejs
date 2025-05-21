5e9d1a01b8011587dd6eda36f16fcb6a513a8c56
deepak78gupta <deepakgupta757600@gmail.com>
2025-05-21 06:22:12 +0530

    make a module and write some functions

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log("Hello World");
+// main.js (Importing Module)
+import { add, subtract } from './utils/mathUtils.js';
+
+console.log(add(5, 3));       // Output: 8
+console.log(subtract(10, 6)); // Output: 4
---
+++ utils/mathUtils.js
@@ -0,0 +1,8 @@
+// mathUtils.js (Module)
+export function add(a, b) {
+    return a + b;
+}
+
+export function subtract(a, b) {
+    return a - b;
+}
